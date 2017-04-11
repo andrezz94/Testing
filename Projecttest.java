9475ef2339124ccdde501af949b48ddbbb7e156b
fnoessse <odelstav.oscar@gmail.com>
2017-04-11 14:48:19 +0200

    Add files via upload

---
+++ Projecttest.java
@@ -0,0 +1,19 @@
+/*
+ */
+
+package projecttest;
+
+/**
+ *
+ * @author Oscar
+ */
+public class Projecttest {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hello world!");
+        
+    }
+
+}
