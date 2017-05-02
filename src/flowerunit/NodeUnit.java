2a9ddef053f8763a694d63de597d4e173158ae89
Oscar <Oscar@Hest>
2017-05-02 02:28:40 +0200

    uppdaterad

--- src/flowerunit/NodeUnit.java
+++ src/flowerunit/NodeUnit.java
@@ -6,10 +6,41 @@ package flowerunit;
  */
 public class NodeUnit {
     
-    private float temp, humidity, soilMoisture;
+    // variablerna är Integers för tillfället, bör senare ändras till flyttal
+    private int temp, humidity, soilMoisture;
     private int Id;
     
+    // huvudkonstruktor för anslutna enheter
     public NodeUnit(int Id){
         this.Id = Id;
+    }
+    
+    // Överskuggad konstruktor för tempvariabel till client uppdateringar
+    public NodeUnit(){
+        
+    }
+    
+    public float getTemp() {
+        return temp;
+    }
+    
+    public void setTemp(int temp) {
+        this.temp = temp;
+    }
+    
+    public float getHumidity() {
+        return humidity;
+    }
+    
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+    }
+    
+    public float getSoilMoisture() {
+        return soilMoisture;
+    }
+    
+    public void setSoilMoisture(int soilMoisture) {
+        this.soilMoisture = soilMoisture;
     }
 }
