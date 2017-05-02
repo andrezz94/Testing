75b4eecd7d5c103695412b4d4ecfa49927c2837f
Oscar <Oscar@Hest>
2017-05-02 04:21:45 +0200

    uppdaterad

--- src/flowerunit/SQL.java
+++ src/flowerunit/SQL.java
@@ -1,7 +1,5 @@
 package flowerunit;
 
-
-
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
@@ -16,19 +14,31 @@ import java.util.logging.Logger;
  * @author Oscar Odelstav, Andre Freberg, Chrstoffer Emilsson
  */
 public class SQL {
-    
+
     private Statement stmt = null;
     private ResultSet rs = null;
     private Connection con = null;
-    
-    public void addValue(Date date, float temp, float humidity, int iD){
-        
+
+    // lägger till ett värde i databasen
+    public void addValue(Date date, int temp, int humidity, int iD) {
+
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql", "username", "password");
+            String driver = "com.mysql.jdbc.Driver";
+            String url = "jdbc:mysql://localhost:3306/mysql";
+            String uName = "username";
+            String passWord = "password";
+
+            Class.forName(driver);
+            con = DriverManager.getConnection(url, uName, passWord);
             stmt = con.createStatement();
-            rs = stmt.executeQuery("Insert * INTO FlowerUnit " + "Values(date, temp, humidity, iD)");
+
+            //stmt.executeUpdate("Insert * INTO FlowerUnit (date, temp, humidity, iD) VALUE ('"+date+"', '"+temp+"', '"+humidity+"', '"+iD+"')");
             
+            // inte testat än men misstänker att executeupdate är för insert och resultset är för select
+            
+            rs = stmt.executeQuery("Insert * INTO FlowerUnit (date, temp, humidity, iD) VALUE ('" + date + "', '" + temp + "', '" + humidity + "', '" + iD + "')");
+            con.close();
+
         } catch (ClassNotFoundException ex) {
             ex.printStackTrace();
             Logger.getLogger(SQL.class.getName()).log(Level.SEVERE, null, ex);
@@ -37,5 +47,10 @@ public class SQL {
             Logger.getLogger(SQL.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
+    // här är det tänkt att returnera alla värden som finns i databasen för en enhet i en lista av något slag
+    protected void getHistory() {
+
+    }
+
 }
