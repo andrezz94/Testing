1a9acd93d4a6ed801c87dfd748b70e242b9591cf
fnoessse <odelstav.oscar@gmail.com>
2017-05-03 03:59:05 +0200

    lägger in senaste arduinofilen och sqlDiagrammet

---
+++ alphaTestDHTtoSQL.ino
@@ -0,0 +1,101 @@
+#include "DHT.h"
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+
+#define DHTPIN 15
+#define DHTTYPE DHT22
+
+float valueToSendTemp, valueToSendHumidity ;
+int iD = 0;
+String request = "addValue";
+
+DHT dht(DHTPIN, DHTTYPE);
+
+ESP8266WiFiMulti WiFiMulti;
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+  pinMode(13, OUTPUT);
+
+    // anslut till wifi
+    WiFiMulti.addAP("AndroidAP", "petc9982");
+
+    Serial.println();
+    Serial.println();
+    Serial.print("Wait for WiFi... ");
+
+    while(WiFiMulti.run() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(500);
+    }
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    delay(500);
+}
+
+void loop() {
+  readSensor();
+  sendToServer();
+  delay(2000);
+      
+}
+
+void sendToServer(){
+
+    const uint16_t port = 8000;
+    const char * host = "192.168.43.100";
+
+    Serial.print("connecting to ");
+    Serial.println(host);
+
+    // Use WiFiClient class to create TCP connections
+    WiFiClient client;
+
+    if (!client.connect(host, port)) {
+        Serial.println("connection failed");
+        Serial.println("wait 5 sec...");
+        delay(5000);
+        return;
+    }
+
+    // This will send the request to the server
+    client.print(request+"\r\n" + iD + valueToSendTemp + "\r\n" + valueToSendHumidity + "\r\n");
+    //client.print(iD);
+    //client.print(valueToSendTemp);
+    //client.print(valueToSendHumidity); 
+    Serial.print("Value sent to server");
+
+    //för att läsa från server
+    //String line = client.readStringUntil('\r');
+    //client.println(line);
+
+    Serial.println("closing connection");
+    client.stop();
+    delay(5000);
+  }
+
+void readSensor(){
+  digitalWrite(13, HIGH);
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  delay(1000);
+  
+  if (isnan(t) || isnan(h)) {
+  Serial.println("Failed to read from DHT");
+  return;
+  } else {
+  digitalWrite(13, LOW);
+  Serial.print("Humidity = ");
+  Serial.println(h);
+  valueToSendHumidity = h;
+  Serial.print("Temperature = ");
+  Serial.println(t);
+  valueToSendTemp = t;
+    }
+  }
+
+
+
