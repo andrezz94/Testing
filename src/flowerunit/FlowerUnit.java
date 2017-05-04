a313ca268d26f0badc4c2754572e3a0ea0636533
Oscar <Oscar@Hest>
2017-05-04 23:27:06 +0200

    uppdaterad 5/3 fungerande alfa verision 1.1

--- src/flowerunit/FlowerUnit.java
+++ src/flowerunit/FlowerUnit.java
@@ -19,12 +19,16 @@ public class FlowerUnit extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
+        
+        
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
         stage.show();
+        
+        
     }
 
     /**
